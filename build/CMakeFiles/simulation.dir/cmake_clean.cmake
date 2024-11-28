16144689a8d15eb61b1fd3e66fa27b2467e006bd
Aladix002 <xbotlo01@stud.fit.vut.cz>
2024-11-28 16:11:14 +0100

    cmake

---
+++ build/CMakeFiles/simulation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulation.dir/ims.cpp.o"
+  "CMakeFiles/simulation.dir/ims.cpp.o.d"
+  "simulation"
+  "simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
